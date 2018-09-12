fd8cda59caf73c737ebdb9f8be14010fe11de787
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-12 21:08:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,12 +3,3 @@ var chocolateBars = [
   "hundred grand",
   "kitkat",
   "skittles"];
-function addElementToBeginningOfArray(1, foo) {
-    var newArray = [1]
-    newArray.push(foo);
-    return newArray;
-      }
-  function destructiveAddElementToBeginningOfArray(array, item) {
-    array.unshift(item);
-    return array;
-  }
