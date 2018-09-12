d623ba81ea6d117aec5d77a8f8890a6ee1d500dc
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-12 21:28:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,4 +6,8 @@ var chocolateBars = [
 function addElementToBeginningOfArray(array,element) {
  return [element, ...array]
 }
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element)
+return array
+}
   
