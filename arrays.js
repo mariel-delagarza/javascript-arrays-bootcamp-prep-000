00f4d82e273abcb3f144f6b1272abd2ad89001c7
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-12 21:13:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = [
   "hundred grand",
   "kitkat",
   "skittles"];
-function addElementToBeginningOfArray() {
+function addElementToBeginningOfArray(array,element) {
   array = [1, foo]
   return array.push(foo)
 }
