94cf1bb01b5c7ef20bea4c05dc116add8055f4f5
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-12 21:37:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array 
 }
 function accessElementInArray(array, index) {
-  console.log(array[index]);
+  return array[index];
 }
   
