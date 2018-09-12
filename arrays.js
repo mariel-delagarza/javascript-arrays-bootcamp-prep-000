e2e16285c4ed1d42d9e44f658f1f21b3aaaec5ff
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-12 20:59:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,12 @@ var chocolateBars = [
   "hundred grand",
   "kitkat",
   "skittles"];
+  function addElementToBeginningOfArray() {
+    dogs = [dog1, dog2, dog3]
+    return ["dog4", ... "dogs"]
+    
+  }
+  function destructiveAddElementToBeginningOfArray() {
+    cats = [cat1, cat2, cat3]
+    return 
+  }
